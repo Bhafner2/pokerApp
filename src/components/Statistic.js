b0c5e7d0b7c15790bd58858645ff2e6d773e3205
AUTEXIS\bha <benjamin.hafner@autexis.com>
2018-11-08 17:21:36 +0100

    LMS

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -169,9 +169,7 @@ class Statistic extends React.Component {
                 return game.won + game.bounty - game.buyIn;
             });
 
-            trends = _.map(filteredGames, (game) => {
-                return (Math.round((game.won - game.buyIn) / 3));
-            });
+            trends = Statistic.lms(totals);
 
             this.setState({
                 sumWon: _.sum(wons),
@@ -210,6 +208,72 @@ class Statistic extends React.Component {
             this.init();
             this.chart(dates, buyIns, wons, totals, bountys, trends, true);
         }
+    }
+
+    static lms(values) {
+        let sum_x = 0;
+        let sum_y = 0;
+        let sum_xy = 0;
+        let sum_xx = 0;
+        let count = 0;
+        let values_x = [];
+
+        for (let i = 0; i < values.length; i++) {
+            values_x.push(i + 1);
+        }
+
+        /*
+        * We'll use those variables for faster read/write access.
+        */
+        let x = 0;
+        let y = 0;
+        let values_length = values_x.length;
+
+        if (values_length !== values.length) {
+            throw new Error('The parameters values_x and values_y need to have same size!');
+        }
+
+        /*
+        * Nothing to do.
+        */
+        if (values_length === 0) {
+            return [[], []];
+        }
+
+        /*
+        * Calculate the sum for each of the parts necessary.
+        */
+        for (let v = 0; v < values_length; v++) {
+            x = values_x[v];
+            y = values[v];
+            sum_x += x;
+            sum_y += y;
+            sum_xx += x * x;
+            sum_xy += x * y;
+            count++;
+        }
+
+        /*
+        * Calculate m and b for the formular:
+        * y = x * m + b
+        */
+        let m = (count * sum_xy - sum_x * sum_y) / (count * sum_xx - sum_x * sum_x);
+        let b = (sum_y / count) - (m * sum_x) / count;
+
+        /*
+        * We will make the x and y result line now
+        */
+        let result_values_x = [];
+        let result_values_y = [];
+
+        for (let v = 0; v < values_length; v++) {
+            x = values_x[v];
+            y = x * m + b;
+            result_values_x.push(x);
+            result_values_y.push(Math.round(y));
+        }
+
+        return result_values_y;
     }
 
     chart(date, buyIn, won, bounty, total, trend, showDots) {
@@ -289,12 +353,14 @@ class Statistic extends React.Component {
                         enabled: showDots,
                     },
                 }, {
-                    name: 'Trend',
+                    name: 'LMS',
                     type: 'spline',
                     data: trend,
                     lineWidth: 1,
                     color: '#007BFF',
-                    visible: false,
+                    /*
+                                        visible: false,
+                    */
                     marker: {
                         enabled: showDots,
                     },
