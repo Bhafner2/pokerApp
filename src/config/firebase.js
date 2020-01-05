9ae816848ba8e9347a9e71cb69035102e8dee3b5
Bhafner2 <hafnerbenjamin@hotmail.com>
2020-01-05 16:36:13 +0100

    config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,14 @@
 // src/firebase.js
 import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyAV7veNM1pCnwaJ3V2Xc09WuYEfL9wWybE",
-    authDomain: "pokerapp-2ed68.firebaseapp.com",
-    databaseURL: "https://pokerapp-2ed68.firebaseio.com",
-    projectId: "pokerapp-2ed68",
-    storageBucket: "pokerapp-2ed68.appspot.com",
-    messagingSenderId: "267167526773"
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
 };
 firebase.initializeApp(config);
 export default firebase; 
